0d98c189322383f039abdec23585b0893e64625a
Abdullah <Abdullah@zenio.se>
2021-05-01 11:14:53 +0200

    Add react aplication to start the projekt

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Contacts from './Contacts'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        GraphQl- Apollo
+      </header>
+        <Contacts/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Contacts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contacts = () => {
+    return (
+        <ul>
+            <li>Abbe</li>
+            <li>Abdullah</li>
+            <li>Abu-alhaijja</li>
+        </ul>
+    )
+}
+export default Contacts;
